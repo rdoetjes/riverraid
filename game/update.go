a34b2512e9c47226a4416815209b99b57d27fcac
Raymond Doetjes <rdoetjes@phonax.com>
2026-09-24 13:36:54 +0200

    cull bullet after 3 seconds

--- game/update.go
+++ game/update.go
@@ -200,13 +200,6 @@ func (g *Game) updateProjectiles(dt float32) {
 		}
 		b.Update(dt)
 
-		// Cull bullet if scrolled off camera
-		if b.Position.Y < g.CameraY-50 || b.Position.Y > g.CameraY+g.ScreenHeight+50 ||
-			b.Position.X < -50 || b.Position.X > g.ScreenWidth+50 {
-			b.SetActive(false)
-			continue
-		}
-
 		g.Bullets[aliveBullets] = b
 		aliveBullets++
 	}
